3502e342b01b056d5964252ebc6a7aa2c00ddd7a
Ilix-studio <ilishjyoti17@gmail.com>
2022-09-30 13:29:59 +0530

    delected

--- client/src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-//i have to used multiple reducer, so combine reducer is used to combime them.
-import alert from "./alert";
-
-export default combineReducers({ alert });
